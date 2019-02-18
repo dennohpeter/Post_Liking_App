f426075857ba3a34a4dc372d2ea9168230a791e3
Peterson <peterson@localhost.localdomain>
2019-02-18 16:01:23 +0300

    Resolved urls conflict

--- post/models.py
+++ post/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    post_heading = models.CharField(max_length=200)
+    post_text = models.TextField()
+
+    def __str__(self):
+        return self.user
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete="")
--- post/views.py
+++ post/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Post, Like
+from django.http import  HttpResponse
 
-# Create your views here.
+def index(request):
+    posts = Post.objects.all() # Getting all posts frm database
+    return render(request, "index.html", {'posts': posts})
+
+
+def likePost(request):
+    if request.method == 'GET':
+        post_id = request.GET['post_id']
+        likedpost = Post.objects.get(pk=post_id) # Getting the liked post
+        m = Like(post=likedpost) # Creating Like Object
+        m.save() #saving it to store in database
+        return HttpResponse("Success!") # Sending an success response
+    else:
+        return HttpResponse("request method is not GET")
